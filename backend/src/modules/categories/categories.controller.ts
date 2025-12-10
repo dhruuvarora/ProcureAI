34ffa06dec0afa85cb61a1d0d990c35d8ef8a418
Dhruv Arora <dhruv862arora@gmail.com>
2025-12-10 13:46:57 +0530

    feat: categories-handler

--- backend/src/modules/categories/categories.controller.ts
+++ backend/src/modules/categories/categories.controller.ts
@@ -4,8 +4,12 @@ import CategoryService from "./categories.service";
 export class CategoriesController {
   async createCategory(req: Request, res: Response) {
     const { category_name } = req.body;
+
     try {
-      const newCategory = await CategoryService.createCategory(category_name);
+      const newCategory = await CategoryService.createCategory({
+        category_name,
+      });
+
       return res.status(201).json({
         success: true,
         message: "Category created successfully",
@@ -17,6 +21,60 @@ export class CategoriesController {
         message: "Something went wrong",
       });
     }
+  }
+
+  async getCategories(req: Request, res: Response) {
+    try {
+      const categories = await CategoryService.getCategories();
+      return res.status(200).json({
+        success: true,
+        data: categories,
+      });
+    } catch (error) {
+      return res.status(500).json({
+        success: false,
+        message: "Something went wrong",
+      });
+    }
+  }
+
+  async updateCategory(req: Request, res: Response) {
+    try {
+      const id = req.params.id;
+
+      const updated = await CategoryService.updateCategory(id, req.body);
+
+      if (!updated) {
+        return res.status(404).json({
+          success: false,
+          message: "Category not found",
+        });
+      }
+
+      return res.status(200).json({
+        success: true,
+        message: "Category updated successfully",
+        data: updated,
+      });
+    } catch (error) {
+      console.error("Error updating category:", error);
+      return res.status(500).json({
+        success: false,
+        message: "Internal server error",
+      });
+    }
+  }
+
+  async deleteCategory(req: Request, res: Response) {
+    const id = req.params.id;
+
+    const deleted = await CategoryService.deleteCategory(id);
+
+    return res.status(200).json({
+      success: true,
+      message: "Category deleted successfully",
+      data: deleted,
+    });
   }
 }
 
--- backend/src/modules/categories/categories.service.ts
+++ backend/src/modules/categories/categories.service.ts
@@ -20,6 +20,48 @@ export class CategoryService {
       throw new Error("Failed to create category: " + error);
     }
   }
+
+  async getCategories() {
+    try {
+      const getCategories = await db
+        .selectFrom("categories")
+        .selectAll()
+        .execute();
+      return getCategories;
+    } catch (error) {
+      throw new Error("Failed to fetch categories: " + error);
+    }
+  }
+
+  async updateCategory(id: string, payload: Partial<createCategoryPayload>) {
+    try {
+      const updateData = {
+        ...(payload.category_name !== undefined && {
+          category_name: payload.category_name,
+        }),
+      };
+
+      const updatedCategory = await db
+        .updateTable("categories")
+        .set(updateData)
+        .where("category_id", "=", id)
+        .returningAll()
+        .executeTakeFirst();
+
+      return updatedCategory;
+    } catch (error) {
+      throw new Error("Failed to update category: " + error);
+    }
+  }
+
+  async deleteCategory(id: string) {
+    try {
+      await db.deleteFrom("categories").where("category_id", "=", id).execute();
+      return true;
+    } catch (error) {
+      throw new Error("Failed to delete category: " + error);
+    }
+  }
 }
 
 export default new CategoryService();
